c077d519486ae141732f26c2424888ccea8b2bbd
MeriDm7 <43909391+MeriDm7@users.noreply.github.com>
2019-08-31 21:56:07 +0300

    Delete pollution.py

--- pollution.py
+++
@@ -1,7 +0,0 @@
-def file_with_pollution(path):
-    f = open(path, encoding='utf-8', errors='ignore')
-    data = f.readlines()
-    lst_with_data = []
-    for i in data:
-        i = i.replace('"', ' ').replace("\t", ' ').replace("\n", " ").replace("'", ' ').split(' ')
-        lst_with_data.append(i)
